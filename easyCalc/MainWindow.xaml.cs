c1f8ea38b56f62a13ba8c86c9aeff64edd8d2b1f
k.okano <okakeru.051212@gmail.com>
2024-03-21 23:02:24 +0900

    四則演算実装完了

--- easyCalc/MainWindow.xaml.cs
+++ easyCalc/MainWindow.xaml.cs
@@ -182,5 +182,90 @@ namespace easyCalc
 
             resultText.Text = result.ToString();
         }
+
+        /// <summary>
+        /// 計算用数字キー、記号キー押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void formulaText_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 押下されたキーの文字を取得
+            string pressKey = e.Key.ToString();
+
+            // テンキー押下時処理追加
+            // 文字の場合は無視
+            if (pressKey == "D0" || pressKey == "NumPad0")
+            {
+                formulaText.Text += "0";
+            }
+            else if (pressKey == "D1" || pressKey == "NumPad1")
+            {
+                formulaText.Text += "1";
+            }
+            else if (pressKey == "D2" || pressKey == "NumPad2")
+            {
+                formulaText.Text += "2";
+            }
+            else if (pressKey == "D3" || pressKey == "NumPad3")
+            {
+                formulaText.Text += "3";
+            }
+            else if (pressKey == "D4" || pressKey == "NumPad4")
+            {
+                formulaText.Text += "4";
+            }
+            else if (pressKey == "D5" || pressKey == "NumPad5")
+            {
+                formulaText.Text += "5";
+            }
+            else if (pressKey == "D6" || pressKey == "NumPad6")
+            {
+                formulaText.Text += "6";
+            }
+            else if (pressKey == "D7" || pressKey == "NumPad7")
+            {
+                formulaText.Text += "7";
+            }
+            else if (pressKey == "D8" || pressKey == "NumPad8")
+            {
+                formulaText.Text += "8";
+            }
+            else if (pressKey == "D9" || pressKey == "NumPad9")
+            {
+                formulaText.Text += "9";
+            }
+            else if (pressKey == "Add")
+            {
+                formulaText.Text += "+";
+            }
+            else if (pressKey == "Subtract")
+            {
+                formulaText.Text += "-";
+            }
+            else if (pressKey == "Multiply")
+            {
+                formulaText.Text += "×";
+            }
+            else if (pressKey == "Divide")
+            {
+                formulaText.Text += "÷";
+            }
+            else if (pressKey == "Return")
+            {
+                // イベントを呼ぶ用のハンドラを作成する
+                var hand = new RoutedEventArgs();
+                var obj = new object();
+
+                // イコールボタン押下時のイベントを呼ぶ
+                equal_Click(obj, hand);
+            }
+
+            // テキストボックス内のカーソル位置を最後尾に設定
+            formulaText.Select(formulaText.Text.Length, 0);
+
+            // keyDownイベントを伝播せずテキスト入力を行う（キーボード入力時2重入力を防ぐ）
+            e.Handled = true;
+        }
     }
 }
